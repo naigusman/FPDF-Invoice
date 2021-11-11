091783c357c0a32e1124ab5518850f056495c7d8
naigusman <94102784+naigusman@users.noreply.github.com>
2021-11-11 13:25:03 +0530

    Add files via upload

---
+++ FPDF/INVOICE/FPDF/tutorial/test.php
@@ -0,0 +1,16 @@
+<?php
+/*call the FPDF library*/
+require('fpdf/fpdf.php');
+
+	
+/*A4 width : 219mm*/
+
+$pdf = new FPDF('P','mm','A4');
+
+$pdf->AddPage();
+/*output the result*/
+
+/*set font to arial, bold, 14pt*/
+$pdf->SetFont('Arial','B',20);
+
+?>
